75ad74c9a1a3797d885ea2620536ad767ae10c1c
kakastg <kakastg9@gmail.com>
2020-09-06 21:52:32 +0200

    Added tools

--- TutorialMod/src/main/java/com/kakastg/tutorial/util/RegistryHandler.java
+++ TutorialMod/src/main/java/com/kakastg/tutorial/util/RegistryHandler.java
@@ -4,9 +4,9 @@ import com.kakastg.tutorial.Tutorial;
 import com.kakastg.tutorial.blocks.BlockItemBase;
 import com.kakastg.tutorial.blocks.RubyBlock;
 import com.kakastg.tutorial.items.ItemBase;
+import com.kakastg.tutorial.tools.ModItemTier;
 import net.minecraft.block.Block;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
+import net.minecraft.item.*;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -25,6 +25,16 @@ public class RegistryHandler {
 
     //Items
     public static final RegistryObject<Item> RUBY = ITEMS.register("ruby", ItemBase::new);
+
+    //Tools
+    public static final RegistryObject<SwordItem> RUBY_SWORD = ITEMS.register("ruby_sword", () ->
+            new SwordItem(ModItemTier.RUBY, 10, 0.0f, new Item.Properties().group(Tutorial.TAB)));
+    public static final RegistryObject<PickaxeItem> RUBY_PICKAXE = ITEMS.register("ruby_pickaxe", () ->
+            new PickaxeItem(ModItemTier.RUBY, 0, 0.0f, new Item.Properties().group(Tutorial.TAB)));
+    public static final RegistryObject<AxeItem> RUBY_AXE = ITEMS.register("ruby_axe", () ->
+            new AxeItem(ModItemTier.RUBY, 15, -3.0f, new Item.Properties().group(Tutorial.TAB)));
+    public static final RegistryObject<ShovelItem> RUBY_SHOVEL = ITEMS.register("ruby_shovel", () ->
+            new ShovelItem(ModItemTier.RUBY, 10, 0.0f, new Item.Properties().group(Tutorial.TAB)));
 
     //Blocks
     public static final RegistryObject<Block> RUBY_BLOCK = BLOCKS.register("ruby_block", RubyBlock::new);
